0339856f4d065cd14ebaea1761dac6eaf042b8d0
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-25 22:26:23 +0800

    remove comments

--- job.js
+++ job.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  // console.log("hi")
 
   let form = document.forms[0]
   form.addEventListener("submit", function(e){
@@ -7,24 +6,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
     console.log(e)
     let inputAll = document.querySelectorAll('input')
     let inputJob = inputAll[0].value
-    // console.log(inputJob)
     let inputLocation = inputAll[1].value
     let inputChecked = inputAll[2].checked
     let inputFullTime = '' 
-    // console.log(inputLocation)
-
-    // ---字串串接---
-    // console.log(inputLocation.split(" "))
-    // console.log(inputLocation.split.length)
-    // let locationStr = inputLocation.split(" ")
-    // // console.log(locationStr)
-    // let newLocationStr = ""
-    // for(let i = 0;  i <= inputLocation.split.length; i++){
-    //   newLocationStr << locationStr[i]
-    //   console.log(newLocationStr)
-    //   // locationStr = inputLocation[i] + '+'
-    // }
-    // console.log(locationStr)
 
     let result = {
       inputJob,
@@ -32,31 +16,19 @@ document.addEventListener('DOMContentLoaded', ()=>{
       inputFullTime,
       url: "https://still-spire-37210.herokuapp.com/positions.json?"
     }
-    console.log(result)
     let searchUrl = ''
 
     if (inputChecked) {
       inputFullTime = 'on',
       searchUrl = result.url + `description=` + inputJob + `&location=` +  inputLocation + `&full_time=` + inputFullTime
-      console.log(searchUrl) 
     } else {
       searchUrl = result.url + `description=` + inputJob + `&location=` +  inputLocation;
-      console.log(searchUrl) 
-      // https://jobs.github.com/positions.json?description=Developer&location=North+York&full_time=on
-      // getJob("ruby", 1);
 
       fetchJob(searchUrl);
       document.getElementsByTagName("BODY")[0].dataset.url = searchUrl
       console.log("URL: "+ document.getElementsByTagName("BODY")[0].dataset.url)
     }
    
-    // {inputJob: "ruby", inputLocation: "us"}
-
-    //https://jobs.github.com/positions.json?utf8=%E2%9C%93&description=ruby&location=us&full_time=on
-
-    // https://jobs.github.com/positions?utf8=%E2%9C%93&description=ruby&location=us&full_time=on
-    
-    // 
   })
 
   document.querySelector('.pagination-next').addEventListener('click',function(e){
@@ -68,10 +40,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
   })
 })
 
-
-
 function fetchJob(searchUrl) {
-  console.log(searchUrl)
   fetch(searchUrl)
   .then(response => response.json())
   .then(jobs => {
